824d528906d0bbd90bc0e2f994553859dc071374
julia stetsenko <julia.nsd.06@gmail.com>
2022-08-31 12:32:40 +0300

    removed unused logic from ServiceResult model

--- MetaExchange.Models/ServiceResult.cs
+++ MetaExchange.Models/ServiceResult.cs
@@ -1,14 +1,10 @@
-﻿using System.Collections.Generic;
-
-namespace MetaExchange.Models
+﻿namespace MetaExchange.Models
 {
     public class ServiceResult
     {
         public bool IsSuccess { get; set; }
 
         public string Message { get; set; }
-
-        public IDictionary<string, IList<string>> Data { get; set; }
 
         public ServiceResult()
         {
@@ -17,28 +13,6 @@ namespace MetaExchange.Models
         public ServiceResult(string message)
         {
             Message = message;
-        }
-
-        public void AddData(string key, string value)
-        {
-            if (Data == null)
-            {
-                Data = new Dictionary<string, IList<string>>();
-            }
-
-            if (Data.TryGetValue(key, out var values))
-            {
-                values.Add(value);
-            }
-            else
-            {
-                values = new List<string>
-                {
-                    value
-                };
-
-                Data.Add(key, values);
-            }
         }
     }
 }
